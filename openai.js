0828d3d267c0c1640d7fb5b78a013cf6f214d3e8
Sergio Prada <chechopradaster@gmail.com>
2023-04-23 18:19:12 -0400

    fix this

--- openai.js
+++ openai.js
@@ -6,13 +6,22 @@ const configuration = new Configuration({
 
 const openai = new OpenAIApi(configuration)
 
-export const prompt = async (prompt) =>
-  openai
-    .createCompletion({
-      model: 'gpt-3.5-turbo',
-      prompt,
-    })
-    .then((res) => res.data.choices[0].text)
+export const prompt = async (prompt) => {
+  const completion = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [{ role: "user", content: prompt }],
+  });
+
+  const completion_text = completion.data.choices[0].message.content;
+  return completion_text
+}
+
+// openai
+//   .createCompletion({
+//     model: 'gpt-3.5-turbo',
+//     prompt,
+//   })
+//   .then((res) => res.data.choices[0].text)
 
 export const promptImage = (prompt, dim = '1024x1024') =>
   openai
@@ -22,3 +31,29 @@ export const promptImage = (prompt, dim = '1024x1024') =>
       size: dim, // 896x912
     })
     .then((res) => res.data.data[0].url)
+
+
+
+// try {
+
+
+//   history.push([user_input, completion_text]);
+
+//   const user_input_again = readlineSync.question(
+//     "\nWould you like to continue the conversation? (Y/N)"
+//   );
+//   if (user_input_again.toUpperCase() === "N") {
+//     return;
+//   } else if (user_input_again.toUpperCase() !== "Y") {
+//     console.log("Invalid input. Please enter 'Y' or 'N'.");
+//     return;
+//   }
+// } catch (error) {
+//   if (error.response) {
+//     console.log(error.response.status);
+//     console.log(error.response.data);
+//   } else {
+//     console.log(error.message);
+//   }
+// }
+//   }
