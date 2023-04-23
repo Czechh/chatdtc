244b9e8b908299a698fe3b3fde353d2c1ac3ab92
Sergio Prada <chechopradaster@gmail.com>
2023-04-23 15:49:40 -0400

    add prompting

--- controllers/business.js
+++ controllers/business.js
@@ -1,10 +1,20 @@
+import { prompt } from '../openai.js';
 import { Business } from '../models.js';
 
 export default class BusinessController {
   static async create(req, res) {
-    const { name, description } = req.body;
+    const {
+      name,
+      description,
+    } = req.body;
     const business = await Business.create({ name, description });
+    const promptRes = prompt(`Given a business with this philosophy return a JSON object with the following Keys:
+      "theme": "string",
+      "product": "string",
+      "targetDemographic": "string"
+      `)
 
+    console.log(promptRes)
     return res.send({ data: business })
   }
 
